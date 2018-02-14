c48fdf1e9ec7056efbfb629e8a5074008bb9fdb8
Johannes Weskamm <weskamm@terrestris.de>
2018-02-14 11:10:52 +0100

    Reuse styles instead of creating new instances

--- terrestrisTiles.js
+++ terrestrisTiles.js
@@ -7,6 +7,11 @@ terrestrisTiles = {};
 // needs to be set before the layer gets created
 terrestrisTiles.useDeclutter = true;
 
+// resources like imagery for highway label signs
+terrestrisTiles.babImg = document.createElement('img');
+terrestrisTiles.babImg.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAbCAYAAADoOQYqAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA3XAAAN1wFCKJt4AAAAB3RJTUUH4gIIDjocExrEmgAAAlVJREFUWMPtmM1OE1EUx393vtoy7dDWpCBgVQJNrGhiYuI7GN2ZuPAdfAV9Bt/BhWufwrAh0UBSQeRToUBhOlOZGWaOiylQjR9BWbRJ/5ubyZ2593fOvZOc/1EiMg68BW4BMSD8oxIRkiQdRQRNKZQCTSk0TfEfUoAOLAGPlIjMA/cvskKcCEEYE0YJQRQThDFBELPQ2Gehscf80h7buz5TYza3p8vcnS1zr3aFopPBMnUypoZlpaOhaxcNYF6JiAfYv3vD9SPafki7E+F1IjwvZO2rx4dPLZY+H7K42mK1cQDtADIGZHQwNVAKROAkgTCGIAZL5+pMmfp0ifrNEvUbRWanHAqOhZ0zcWyTwoiFY5t/OhlfiYgLFPzjE740O+wfHbPb+obnBuwcHPNx84jlrTYrGy4rmy40fTC0FM7QQVfps/rLxTqdjxOIfwqmnGVy0qFWHWV6okDtmsNUxSY3YlEpZ6mUckxUbOysAdBWichRY+3QefHqHdtuwHazw8aOT9j0081MDUwdNJUCnmZAuBwpIAGSnmCiGBKBUo7qeJ7qeJ6p0Qwvnz+gdr3oGiLCh+UWb16/h7E8aKRgxeyvN7ks2N71FKBr6a9m6YDZnRPWt9qsb7iw4/HkYY3Z6ijG2Yd5K81qP0mpNBBdpXzdhGk/3Ld+1umJnEEPmIbQQ+gh9BB6CD2EHkIPOHRPMdK36inqzqs8L4QoSZ2F9EnJJ5IagyhJ+bqJNZRSzM2UePrsTn86l8lz5zI3U0IpxWB6xEF14wPZ9xjrdpjqA9BhWgQefwcq3oy642N9sQAAAABJRU5ErkJggg==';
+
+
 // the OSM-attribution
 terrestrisTiles.attribution = new ol.Attribution({
   html: '© terrestris GmbH & Co. KG<br>' +
@@ -29,6 +34,63 @@ terrestrisTiles.labelStyle = new ol.style.Style({
     maxAngle: 0.4,
     padding: [10, 10, 10, 10]
   })
+});
+
+// the style template used for linestrings
+terrestrisTiles.lineStringStyleBelow = new ol.style.Style({
+  stroke: new ol.style.Stroke({
+    color: 'black',
+    width: 2,
+    lineCap: 'butt',
+    lineDash: []
+  }),
+  zIndex: 0
+});
+
+// the style template used for linestrings
+terrestrisTiles.lineStringStyleAbove = new ol.style.Style({
+  stroke: new ol.style.Stroke({
+    color: 'white',
+    width: 1,
+    lineCap: 'round',
+    lineDash: []
+  }),
+  zIndex: 1
+});
+
+// the style template used for polygons
+terrestrisTiles.polygonStyle = new ol.style.Style({
+  fill: new ol.style.Fill({
+    color: 'blue'
+  }),
+  stroke: new ol.style.Stroke({
+    color: 'blue',
+    width: 1,
+    lineCap: 'round'
+  }),
+  zIndex: 0
+});
+
+// the style template used for highway icons
+terrestrisTiles.iconStyle = new ol.style.Style({
+  image: new ol.style.Icon({
+    scale: 0.9,
+    imgSize: [45,27],
+    img: terrestrisTiles.babImg,
+    opacity: 0.8
+  }),
+  text: new ol.style.Text({
+    padding: [50, 50, 50, 50],
+    font: '12px sans-serif',
+    fill: new ol.style.Fill({
+      color: '#fff'
+    }),
+    stroke: new ol.style.Stroke({
+      color: '#fff',
+      width: 0.8
+    })
+  }),
+  zIndex: 0
 });
 
 /**
@@ -56,14 +118,9 @@ terrestrisTiles.buildLabelStyle = function(feature, resolution) {
   }
   fontString = fontSize + fontString;
   var olText = terrestrisTiles.labelStyle.getText();
-  olText.setFill(new ol.style.Fill({
-    color: '#000',
-    width: 3
-  }));
-  olText.setStroke(new ol.style.Stroke({
-    color: '#fff',
-    width: 5
-  }));
+  olText.getFill().setColor('#000');
+  olText.getStroke().setColor('#fff');
+  olText.getStroke().setWidth(5);
   olText.setPlacement('point');
   olText.setText(text);
   olText.setFont(fontString);
@@ -97,64 +154,58 @@ terrestrisTiles.buildStyle = function(feature, resolution, styleArray, geom) {
       styleToUse = el.style;
     }
   });
+
   if (!styleToUse || !styleToUse[type]) {
     return null;
   }
+
   var style = [];
+  var stroke;
+  var strokeAbove;
+  var fill;
+  var color;
   if (geom === 'line') {
-    var below = new ol.style.Style({
-      stroke: new ol.style.Stroke({
-        color: styleToUse[type].colors[0] || 'black',
-        width: styleToUse[type].widths[0] || 2,
-        lineCap: styleToUse[type].caps[0] || 'butt',
-        opacity: styleToUse[type].opacity || 1,
-        lineDash: styleToUse[type].dasharray ?
-          styleToUse[type].dasharray.split(' ') : []
-      }),
-      zIndex: feature.get('z_order') || 0
-    });
-    style.push(below);
+    stroke = terrestrisTiles.lineStringStyleBelow.getStroke();
+    stroke.setColor(styleToUse[type].colors[0] || 'black');
+    stroke.setWidth(styleToUse[type].widths[0] || 2);
+    stroke.setLineCap(styleToUse[type].caps[0] || 'butt');
+    stroke.setLineDash(styleToUse[type].dasharray ?
+      styleToUse[type].dasharray.split(' ') : []);
+    terrestrisTiles.lineStringStyleBelow.setZIndex(feature.get('z_order') || 0);
+    style.push(terrestrisTiles.lineStringStyleBelow);
 
     // check if we need a "outline" for this road
     if (styleToUse[type].widths.length > 1) {
-      var above = new ol.style.Style({
-        stroke: new ol.style.Stroke({
-          color: styleToUse[type].colors[1] || 'white',
-          width: styleToUse[type].widths[1] || 1,
-          lineCap: styleToUse[type].caps[1] || 'round',
-          opacity: styleToUse[type].opacity || 1
-        }),
-        zIndex: feature.get('z_order') + 1 || 1
-      });
-      style.push(above);
+      strokeAbove = terrestrisTiles.lineStringStyleAbove.getStroke();
+      strokeAbove.setColor(styleToUse[type].colors[1] || 'white');
+      strokeAbove.setWidth(styleToUse[type].widths[1] || 1);
+      strokeAbove.setLineCap(styleToUse[type].caps[1] || 'round');
+      strokeAbove.setLineDash(styleToUse[type].dasharray ?
+        styleToUse[type].dasharray.split(' ') : []);
+      terrestrisTiles.lineStringStyleAbove.setZIndex(feature.get('z_order') + 1 || 1);
+      style.push(terrestrisTiles.lineStringStyleAbove);
     }
   } else if (geom === 'polygon') {
-    var polyStyle = new ol.style.Style({
-      fill: (styleToUse[type].fillColor || styleToUse[type].fillOpacity) ?
-        new ol.style.Fill({
-          color: styleToUse[type].fillColor || 'blue'
-        }) : undefined,
-      stroke: (styleToUse[type].strokeColor || styleToUse[type].strokeWidth) ?
-        new ol.style.Stroke({
-          color: styleToUse[type].strokeColor || 'blue',
-          width: styleToUse[type].strokeWidth || 1,
-          lineCap: styleToUse[type].lineCap || 'round'
-        }) : undefined,
-      zIndex: styleToUse[type].zIndex || (feature.get('z_order') || 0)
-    });
+    fill = terrestrisTiles.polygonStyle.getFill();
+    fill.setColor(styleToUse[type].fillColor || 'blue');
+    stroke = terrestrisTiles.polygonStyle.getStroke();
+    stroke.setColor(styleToUse[type].strokeColor || 'blue');
+    stroke.setWidth(styleToUse[type].strokeWidth || 1);
+    stroke.setLineCap(styleToUse[type].lineCap || 'round');
+    terrestrisTiles.polygonStyle.setZIndex(styleToUse[type].zIndex || feature.get('z_order') || 0);
+
     // set the alpha values
-    var color;
-    if (polyStyle.getFill() && styleToUse[type].fillOpacity) {
-      color = ol.color.asArray(polyStyle.getFill().getColor());
+    if (fill && styleToUse[type].fillOpacity) {
+      color = ol.color.asArray(fill.getColor());
       color[3] = styleToUse[type].fillOpacity;
-      polyStyle.getFill().setColor(color);
+      fill.setColor(color);
     }
-    if (polyStyle.getStroke() && styleToUse[type].strokeOpacity) {
-      color = ol.color.asArray(polyStyle.getStroke().getColor());
+    if (stroke && styleToUse[type].strokeOpacity) {
+      color = ol.color.asArray(stroke.getColor());
       color[3] = styleToUse[type].strokeOpacity;
-      polyStyle.getStroke().setColor(color);
+      stroke.setColor(color);
     }
-    style.push(polyStyle);
+    style.push(terrestrisTiles.polygonStyle);
   }
 
   if (styleToUse[type].useLabels !== false) {
@@ -163,42 +214,18 @@ terrestrisTiles.buildStyle = function(feature, resolution, styleArray, geom) {
       if (feature.getGeometry().b && feature.getGeometry().b.length > 1) {
         var coords = feature.getGeometry().b;
         var point = new ol.geom.Point([coords[0], coords[1]]);
-        terrestrisTiles.labelStyle.getText().setPlacement('point');
-        var iconStyle = new ol.style.Style({
-          geometry: point,
-          image: new ol.style.Icon({
-            scale: 0.9,
-            imgSize: [45,27],
-            img: babImg,
-            opacity: 0.8
-          }),
-          text: new ol.style.Text({
-            padding: [50, 50, 50, 50],
-            text: text,
-            font: '12px sans-serif',
-            fill: new ol.style.Fill({
-              color: '#fff'
-            }),
-            stroke: new ol.style.Stroke({
-              color: '#fff',
-              width: 0.8
-            })
-          }),
-          zIndex: 0
-        });
-        style.push(iconStyle);
+        terrestrisTiles.iconStyle.setGeometry(point);
+        terrestrisTiles.iconStyle.getText().setText(text);
+        style.push(terrestrisTiles.iconStyle);
       }
     } else if (resolution < 300) {
       // care about labels for streets, buildings etc.
       var olText = terrestrisTiles.labelStyle.getText();
-      olText.setFill(new ol.style.Fill({
-        color: styleToUse[type].textFillColor || 'black',
-        width: styleToUse[type].textFillWidth || 3
-      }));
-      olText.setStroke(new ol.style.Stroke({
-        color: styleToUse[type].textStrokeColor || 'white',
-        width: styleToUse[type].textStrokeWidth || 5
-      }));
+      fill = olText.getFill();
+      stroke = olText.getStroke();
+      fill.setColor(styleToUse[type].textFillColor || 'black');
+      stroke.setColor(styleToUse[type].textStrokeColor || 'white');
+      stroke.setWidth(styleToUse[type].textStrokeWidth || 5);
       olText.setPlacement(styleToUse[type].textPlacement || 'line');
       olText.setText(text);
       olText.setFont(styleToUse[type].font || '13px sans-serif');
@@ -275,7 +302,3 @@ terrestrisTiles.getOSMLayer = function() {
   });
   return osmLayer;
 };
-
-// resources like imagery for highway label signs
-var babImg = document.createElement('img');
-babImg.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAbCAYAAADoOQYqAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAA3XAAAN1wFCKJt4AAAAB3RJTUUH4gIIDjocExrEmgAAAlVJREFUWMPtmM1OE1EUx393vtoy7dDWpCBgVQJNrGhiYuI7GN2ZuPAdfAV9Bt/BhWufwrAh0UBSQeRToUBhOlOZGWaOiylQjR9BWbRJ/5ubyZ2593fOvZOc/1EiMg68BW4BMSD8oxIRkiQdRQRNKZQCTSk0TfEfUoAOLAGPlIjMA/cvskKcCEEYE0YJQRQThDFBELPQ2Gehscf80h7buz5TYza3p8vcnS1zr3aFopPBMnUypoZlpaOhaxcNYF6JiAfYv3vD9SPafki7E+F1IjwvZO2rx4dPLZY+H7K42mK1cQDtADIGZHQwNVAKROAkgTCGIAZL5+pMmfp0ifrNEvUbRWanHAqOhZ0zcWyTwoiFY5t/OhlfiYgLFPzjE740O+wfHbPb+obnBuwcHPNx84jlrTYrGy4rmy40fTC0FM7QQVfps/rLxTqdjxOIfwqmnGVy0qFWHWV6okDtmsNUxSY3YlEpZ6mUckxUbOysAdBWichRY+3QefHqHdtuwHazw8aOT9j0081MDUwdNJUCnmZAuBwpIAGSnmCiGBKBUo7qeJ7qeJ6p0Qwvnz+gdr3oGiLCh+UWb16/h7E8aKRgxeyvN7ks2N71FKBr6a9m6YDZnRPWt9qsb7iw4/HkYY3Z6ijG2Yd5K81qP0mpNBBdpXzdhGk/3Ld+1umJnEEPmIbQQ+gh9BB6CD2EHkIPOHRPMdK36inqzqs8L4QoSZ2F9EnJJ5IagyhJ+bqJNZRSzM2UePrsTn86l8lz5zI3U0IpxWB6xEF14wPZ9xjrdpjqA9BhWgQefwcq3oy642N9sQAAAABJRU5ErkJggg==';
